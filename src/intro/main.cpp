f2b4e6a83cc940ae2dd2ce70025a3bf65fc07283
Grzegorz <tetsuakeeru@gmail.com>
2024-02-21 01:19:59 +0100

    Add: lib usage in exe

--- src/intro/main.cpp
+++ src/intro/main.cpp
@@ -1,10 +1,14 @@
 #include <intro_lib.hpp>
+#include <iterator>
+#include <span>
 #include <spdlog/spdlog.h>
 
 
 int main(int argc, const char **argv)
 {
-    spdlog::info("AL: {}\nArg[0]: {}", argc, argv[0]);
+    auto args = std::span(argv, size_t(argc));
 
+    spdlog::info("AL: {}\nArg[0]: {}", argc, args[0]);
+    spdlog::info("intro_lib::factorial_constexpr(4): {}", intro_lib::factorial_constexpr(4));
     return 0;
 }
